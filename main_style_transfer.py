90ccfb4424d992605a0887e167fe93d39bc7a965
pedrops164 <A95826@alunos.uminho.pt>
2024-05-02 22:05:25 +0100

    mudnacas

--- main_style_transfer.py
+++ main_style_transfer.py
@@ -29,6 +29,8 @@ def get_args_parser():
     parser.add_argument('--lr_drop', default=200, type=int)
     parser.add_argument('--clip_max_norm', default=0.1, type=float,
                         help='gradient clipping max norm')
+    parser.add_argument('--in_content_folder', default=None, type=str)
+    parser.add_argument('--style_folder', default=None, type=str)
 
     # Model parameters
     parser.add_argument('--frozen_weights', type=str, default=None,
